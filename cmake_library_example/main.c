6ea1c7d4a70ce4d687d5ddd2c71a9932e57c0796
onurbstnc <meh.bstnc@gmail.com>
2023-05-20 17:26:12 +0300

    cmake examples added

---
+++ cmake_library_example/main.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+
+int main()
+{
+
+    printf("This is a creating library with CMake example .. \n");
+    return 0;
+}
---
+++ simple_cmake_example/main.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+
+int main()
+{
+
+    printf("Testt \n");
+    return 0;
+}
